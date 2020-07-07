9aad78152d9a59e6048ee0e831a5a5124f455b81
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-07-07 17:35:42 +0300

    исправить ошибки

---
+++ javascript/index.js
@@ -0,0 +1,29 @@
+let openEditButton = document.querySelector('.profile__edit-button');
+let closeEditButton = document.querySelector('.modal__close-button');
+let modal = document.querySelector('.modal');
+let form = modal.querySelector('.modal__form');
+let inputName = document.querySelector('.form__item_type_name');
+let inputProfession = document.querySelector('.form__item_type_profession');
+let nameText = document.querySelector('.profile__title');
+let professionText = document.querySelector('.profile__subtitle');
+
+function openModal() {
+  modal.classList.add('modal_open');
+}
+
+function closeModal() {
+  modal.classList.remove('modal_open');
+  inputName.value = nameText.innerText;
+  inputProfession.value = professionText.innerText;
+}
+
+function saveModal(event) {
+  event.preventDefault(); 
+  nameText.textContent = inputName.value;
+  professionText.textContent = inputProfession.value;
+  closeModal();
+}
+
+form.addEventListener('submit', saveModal);
+openEditButton.addEventListener('click', openModal);
+closeEditButton.addEventListener('click', closeModal);
