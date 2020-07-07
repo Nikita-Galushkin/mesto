a8bbb0f668ca551c771942aba9f6ae19d7806758
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-07-07 21:34:51 +0300

    добавить type кнопкам

--- javascript/index.js
+++ javascript/index.js
@@ -2,19 +2,19 @@ let openEditButton = document.querySelector('.profile__edit-button');
 let closeEditButton = document.querySelector('.modal__close-button');
 let modal = document.querySelector('.modal');
 let form = modal.querySelector('.modal__form');
-let inputName = document.querySelector('.form__item_type_name');
-let inputProfession = document.querySelector('.form__item_type_profession');
+let inputName = document.querySelector('.modal__item_type_name');
+let inputProfession = document.querySelector('.modal__item_type_profession');
 let nameText = document.querySelector('.profile__title');
 let professionText = document.querySelector('.profile__subtitle');
 
 function openModal() {
   modal.classList.add('modal_open');
+  inputName.value = nameText.innerText;
+  inputProfession.value = professionText.innerText;
 }
 
 function closeModal() {
   modal.classList.remove('modal_open');
-  inputName.value = nameText.innerText;
-  inputProfession.value = professionText.innerText;
 }
 
 function saveModal(event) {
