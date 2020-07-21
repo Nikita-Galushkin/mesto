76a84cff11dd452180f34fc43aa5d05018e7f888
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-07-21 12:33:01 +0300

    remove double code

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,31 @@
-let openEditButton = document.querySelector('.profile__edit-button');
-let closeEditButton = document.querySelector('.modal__close-button_type_edit');
-let editModal = document.querySelector('.modal_type_edit-profile');
-let editForm = editModal.querySelector('.modal__form_type_edit');
+const openEditButton = document.querySelector('.profile__edit-button');
+const closeEditButton = document.querySelector('.modal__close-button_type_edit');
+const editModal = document.querySelector('.modal_type_edit-profile');
+const editForm = editModal.querySelector('.modal__form_type_edit');
 
-let openAddButton = document.querySelector('.profile__add-button');
-let closeAddButton = document.querySelector('.modal__close-button_type_add');
-let addModal = document.querySelector('.modal_type_add-card');
-let addForm = addModal.querySelector('.modal__form_type_add');
+const openAddButton = document.querySelector('.profile__add-button');
+const closeAddButton = document.querySelector('.modal__close-button_type_add');
+const addModal = document.querySelector('.modal_type_add-card');
+const addForm = addModal.querySelector('.modal__form_type_add');
 
-let closePhotoButton = document.querySelector('.modal__close-button_type_photo');
-let photoModal = document.querySelector('.modal_type_photo');
-let openPhoto = document.querySelector('.modal__photo');
-let textPhoto = document.querySelector('.modal__text');
+const closePhotoButton = document.querySelector('.modal__close-button_type_photo');
+const photoModal = document.querySelector('.modal_type_photo');
+const openPhoto = document.querySelector('.modal__photo');
+const textPhoto = document.querySelector('.modal__text');
 
-let inputName = document.querySelector('.modal__item_type_name');
-let inputProfession = document.querySelector('.modal__item_type_profession');
+const inputName = document.querySelector('.modal__item_type_name');
+const inputProfession = document.querySelector('.modal__item_type_profession');
 
-let inputPlace = document.querySelector('.modal__item_type_place');
-let inputLink = document.querySelector('.modal__item_type_link-place');
+const inputPlace = document.querySelector('.modal__item_type_place');
+const inputLink = document.querySelector('.modal__item_type_link-place');
 
-let nameText = document.querySelector('.profile__title');
-let professionText = document.querySelector('.profile__subtitle');
+const nameText = document.querySelector('.profile__title');
+const professionText = document.querySelector('.profile__subtitle');
 
-let elementTemplate = document.querySelector('#element-template').content;
-let elementContainer = document.querySelector('.elements__list');
+const elementTemplate = document.querySelector('#element-template').content;
+const elementContainer = document.querySelector('.elements__list');
 
-
-let initialCards = [
+const initialCards = [
   {
       name: 'Архыз',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -54,18 +53,6 @@ let initialCards = [
 ];
 
 
-for (let i = 0; i < initialCards.length; i++) {
-  let cardElement = elementTemplate.cloneNode(true);
-  cardElement.querySelector('.element__image').src = initialCards[i].link;
-  cardElement.querySelector('.element__image').alt = initialCards[i].name;
-  cardElement.querySelector('.element__title').textContent = initialCards[i].name;
-  cardElement.querySelector('.element__group').addEventListener('click', toggleLike);
-  cardElement.querySelector('.element__trash').addEventListener('click', removeElement);
-  cardElement.querySelector('.element__photo').addEventListener('click', openPhotoModal);
-  cardElement.querySelector('.element__photo').addEventListener('click', photoElement);
-  addCard(cardElement);
-}
-
 function addCard(card) {
   elementContainer.prepend(card);
 }
@@ -83,66 +70,76 @@ function photoElement(evt) {
   textPhoto.textContent = evt.target.getAttribute('alt');
 }
 
-
-function openEditModal() {
-  editModal.classList.add('modal_open');
-  inputName.value = nameText.innerText;
-  inputProfession.value = professionText.innerText;
+function clearValue() {
+  inputPlace.value = '';
+  inputLink.value = '';
 }
 
-function closeEditModal() {
-  editModal.classList.remove('modal_open');
+function toggleModal(modal) {
+  modal.classList.toggle('modal_open');
+}
+
+function newCard(link, name) {
+  const cardElement = elementTemplate.cloneNode(true);
+  const elementImage = cardElement.querySelector('.element__image');
+  const elementPhoto = cardElement.querySelector('.element__photo');
+  elementImage.src = link;
+  elementImage.alt = name;
+  cardElement.querySelector('.element__title').textContent = name;
+  cardElement.querySelector('.element__group').addEventListener('click', toggleLike);
+  cardElement.querySelector('.element__trash').addEventListener('click', removeElement);
+  elementPhoto.addEventListener('click', togglePhotoModal);
+  elementPhoto.addEventListener('click', photoElement);
+  addCard(cardElement);
+}
+
+initialCards.forEach(function (item) {
+  newCard(item.link, item.name);
+});
+
+function toggleEditModal() {
+  toggleModal(editModal);
+}
+
+function toggleAddModal() {
+  toggleModal(addModal);
+}
+
+function togglePhotoModal() {
+  toggleModal(photoModal);
+}
+
+function closeAddModal() {
+  toggleAddModal();
+  clearValue();
+}
+
+function openEditModal() {
+  toggleEditModal();
+  inputName.value = nameText.innerText;
+  inputProfession.value = professionText.innerText;
 }
 
 function saveEditModal(event) {
   event.preventDefault(); 
   nameText.textContent = inputName.value;
   professionText.textContent = inputProfession.value;
-  closeEditModal();
-}
-
-
-function openAddModal() {
-  addModal.classList.add('modal_open');
-}
-
-function closeAddModal() {
-  addModal.classList.remove('modal_open');
-  inputPlace.value = '';
-  inputLink.value = '';
+  toggleEditModal();
 }
 
 function saveAddModal(event) {
   event.preventDefault();
-  let newCardElement = elementTemplate.cloneNode(true);
-  newCardElement.querySelector('.element__image').src = inputLink.value;
-  newCardElement.querySelector('.element__title').textContent = inputPlace.value;
-  newCardElement.querySelector('.element__image').alt = inputPlace.value;
-  newCardElement.querySelector('.element__group').addEventListener('click', toggleLike);
-  newCardElement.querySelector('.element__trash').addEventListener('click', removeElement);
-  newCardElement.querySelector('.element__photo').addEventListener('click', openPhotoModal);
-  newCardElement.querySelector('.element__photo').addEventListener('click', photoElement);
-  addCard(newCardElement);
-  inputPlace.value = '';
-  inputLink.value = '';
+  newCard(inputLink.value, inputPlace.value);
+  clearValue();
   closeAddModal();
 }
 
-
-function openPhotoModal() {
-  photoModal.classList.add('modal_open');
-}
-
-function closePhotoModal() {
-  photoModal.classList.remove('modal_open');
-}
-
 openEditButton.addEventListener('click', openEditModal);
-closeEditButton.addEventListener('click', closeEditModal);
+closeEditButton.addEventListener('click', toggleEditModal);
 editForm.addEventListener('submit', saveEditModal);
 
-openAddButton.addEventListener('click', openAddModal);
+openAddButton.addEventListener('click', toggleAddModal);
 closeAddButton.addEventListener('click', closeAddModal);
 addForm.addEventListener('submit', saveAddModal);
 
-closePhotoButton.addEventListener('click', closePhotoModal);
+closePhotoButton.addEventListener('click', togglePhotoModal);
